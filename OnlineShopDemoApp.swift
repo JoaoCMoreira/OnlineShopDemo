e7fe7a0d2acfb7126365714cf2767836158fa522
Joao Moreira <moreira.joaoc@gmail.com>
2023-06-16 00:30:41 +0100

    Initial commit

---
+++ OnlineShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopDemoApp.swift
+//  OnlineShopDemo
+//
+//  Created by João Moreira on 16/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
